f559cf34dbcf13579f02baa51ed09ea882adae95
lappet69 <sinabariba.andre@gmail.com>
2023-05-23 15:19:46 +0700

    update

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -23,10 +23,14 @@ const Login = () => {
   return loading ? (
     <Loader />
   ) : (
-    <div className="flex flex-col h-screen bg-[#f4f4f4]  justify-center items-center">
-      <div className="flex w-full max-w-5xl gap-5 justify-center p-12 rounded shadow-xl border bg-white relative">
+    <div className="flex flex-col h-screen bg-white md:bg-[#f4f4f4]  justify-center items-center px-6  ">
+      <div className="flex w-full max-w-5xl gap-5 justify-center p-12 rounded shadow-lg  border bg-white relative">
         <img src={Logo} alt="logo" className="m-auto absolute top-4" />
-        <img src={bgLogin} alt="logo" className=" max-w-sm drop-shadow-2xl" />
+        <img
+          src={bgLogin}
+          alt="logo"
+          className="hidden md:block max-w-sm drop-shadow-2xl"
+        />
         <div className="w-full max-w-lg  px-14 bg-white py-12 rounded ">
           <p className="text-center text-xl mb-6">Welcome</p>
           <form className="flex flex-col" onSubmit={handleLogin}>
