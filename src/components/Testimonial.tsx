fe5e5a548670015eae57779e77bea1bd2942d1ab
lappet69 <sinabariba.andre@gmail.com>
2023-05-23 14:45:32 +0700

    update

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -15,15 +15,16 @@ const Testimonial = () => {
       </div>
       <Clients />
       <div className="flex justify-center relative ">
-        <div className="w-full flex flex-col bg-primaryColor border border-black rounded-lg mx-24  items-center py-6 gap-6">
-          <p>Is software important for your business?</p>
-          <p className="text-3xl ">Build it with Albatech</p>
-          <button className="outline outline-black outline-1 px-10 py-3 rounded-full font-semibold">
+        <div className="w-full flex flex-col sm:bg-primaryColor sm:border border-black rounded-lg mx-24  items-center py-6 gap-6">
+          <p className="text-center">
+            Is software important for your business?
+          </p>
+          <p className="text-3xl text-center">Build it with Albatech</p>
+          <button className="outline outline-primaryColor hover:bg-primaryColor sm:outline-black outline-1 px-10 py-3 rounded-full font-semibold">
             Consultant Now
           </button>
         </div>
       </div>
-      {/* <hr className="  mt-10 w-10/12 m-auto" /> */}
     </div>
   );
 };
