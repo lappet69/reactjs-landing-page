eb51a4c814054c44cd293906b2de7944dab335d9
lappet69 <sinabariba.andre@gmail.com>
2023-05-23 14:55:54 +0700

    update

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { BsArrowRight } from "react-icons/bs";
 import { IPackage } from "../interface";
 
 interface Props {
@@ -5,15 +7,20 @@ interface Props {
 }
 
 const Card = ({ packages }: Props) => {
+  const [isHover, setIsHover] = useState(false);
   return (
     <div className="h-full rounded-lg border border-[gray/75] p-10 md:p-4 lg:p-5 justify-between gap-5 cursor-pointer hover:border-primaryColor">
-      <div className="flex flex-col  items-center md:items-start">
+      <div
+        className="flex flex-col  items-center md:items-start"
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
         <div className="h-15 md:w-15 ">
           <img src={packages.img} alt="" className="h-full" />
         </div>
         <div className="flex flex-col h-full items-center text-center md:text-left md:items-start mt-6">
-          <p className="flex md:text-lg lg:text-xl text-primaryColor font-semibold ">
-            {packages.title}
+          <p className="flex md:text-lg lg:text-xl text-primaryColor font-semibold items-center gap-2">
+            {packages.title} {isHover && <BsArrowRight />}
           </p>
           <p>{packages.desc}</p>
         </div>
